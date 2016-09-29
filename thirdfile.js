4deec2794a8110c510f2c32d021cab7535e2ee8d
xeccvzs <xeccvzs@bnymellon.com>
2016-09-29 20:40:59 +0530

    test123

--- thirdfile.js
+++ thirdfile.js
@@ -1,8 +1,4 @@
 alert('helloworld');
 
-<<<<<<< HEAD
 alert('alert from jir123 br123');
 alert('conflicts expected');
-=======
-alert('alert from jir123 branch');
->>>>>>> 582e6d77e047bbf4d07952f406d61a677f062392
